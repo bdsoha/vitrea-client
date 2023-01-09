c79612baeae0f44f69240bfced8120d64b6abc39
Dov Benyomin Sohacheski <b@kloud.email>
2023-01-09 10:53:10 +0000

    📜 Added logger interface, closes #4

--- src/VitreaClient.ts
+++ src/VitreaClient.ts
@@ -7,37 +7,39 @@ import { ToggleHeartBeat }             from './requests/ToggleHeartBeat'
 import { TimeoutException }            from './socket/TimeoutException'
 import { SplitMultipleBuffers }        from './utilities/SplitMultipleBuffers'
 import { VitreaHeartbeatHandler }      from './socket/VitreaHeartbeatHandler'
-import { BaseRequest, BaseResponse }   from './core'
 import { VBoxConfigs, VBoxConnection } from './utilities/VBoxConnection'
 import Net                             from 'net'
+import * as Core                       from './core'
 
 export class VitreaClient extends AbstractSocket {
     protected readonly mutex = new Mutex()
-    protected readonly configs : VBoxConfigs
+    protected readonly configs: VBoxConfigs
     protected readonly version: ProtocolVersion
+    protected readonly log: Core.LoggerContract
 
-    public constructor(configs: Required<VBoxConfigs>) {
+    public constructor(configs: Required<VBoxConfigs>, logger: Core.LoggerContract = null) {
         super(configs.host, configs.port)
-        this.configs  = configs
+        this.configs = configs
         this.heartbeat = VitreaHeartbeatHandler.create(this)
+        this.log = logger ?? new Core.NullLogger()
     }
 
-    public async send<T extends BaseRequest, R extends BaseResponse>(request : T) : Promise<R> {
-        // const eventName = {eventName: request.eventName}
+    public async send<T extends Core.BaseRequest, R extends Core.BaseResponse>(request: T): Promise<R> {
+        const eventName = { eventName: request.eventName }
 
-        // this.log.debug('Waiting for mutex', eventName)
+        this.log.debug('Waiting for mutex', eventName)
         const release = await this.mutex.acquire()
-        // this.log.debug('Acquired mutex', eventName)
+        this.log.debug('Acquired mutex', eventName)
 
         return new Promise((res, rej) => {
-            // this.log.info('Sending data', request.logData)
+            this.log.info('Sending data', request.logData)
 
-            let callback : (data : R) => void
+            let callback: (data: R) => void
 
             const timeout = setTimeout(() => {
                 const message = 'Sending timeout reached'
 
-                // this.log.error(message, eventName)
+                this.log.error(message, eventName)
 
                 this.removeListener(request.eventName, callback)
 
@@ -60,7 +62,7 @@ export class VitreaClient extends AbstractSocket {
         })
     }
 
-    protected async onConnect(socket : Net.Socket) {
+    protected async onConnect(socket: Net.Socket) {
         await super.onConnect(socket)
 
         await this.send(new ToggleHeartBeat())
@@ -68,7 +70,7 @@ export class VitreaClient extends AbstractSocket {
         await this.send(new Login(this.configs.username, this.configs.password))
     }
 
-    protected onData(data : Buffer) : void {
+    protected onData(data: Buffer): void {
         const split = SplitMultipleBuffers.handle(data)
 
         if (split.length > 1) {
@@ -80,11 +82,11 @@ export class VitreaClient extends AbstractSocket {
         const response = ResponseFactory.find(data, this.version)
 
         if (response) {
-            // this.log.info('Data Received', response.logData)
+            this.log.info('Data Received', response.logData)
 
             this.emit(response.eventName, response)
         } else {
-            // this.log.warn('Ignoring unrecognized received data', {raw: data.toString('hex')})
+            this.log.warn('Ignoring unrecognized received data', { raw: data.toString('hex') })
         }
     }
 
--- src/socket/AbstractSocket.ts
+++ src/socket/AbstractSocket.ts
@@ -1,9 +1,9 @@
-import { EventEmitter }     from 'events'
-import { TimeoutException } from './TimeoutException'
-// import {getLogger, Logger}        from '@/core/logger'
-import { WritableSocketContract }   from './WritableSocketContract'
-import { AbstractHeartbeatHandler } from './AbstractHeartbeatHandler'
-import * as Net                     from 'net'
+import { EventEmitter }               from 'events'
+import { TimeoutException }           from './TimeoutException'
+import { WritableSocketContract }     from './WritableSocketContract'
+import { AbstractHeartbeatHandler }   from './AbstractHeartbeatHandler'
+import { LoggerContract, NullLogger } from '../core'
+import * as Net                       from 'net'
 
 export abstract class AbstractSocket extends EventEmitter implements WritableSocketContract {
     protected port : number
@@ -11,24 +11,24 @@ export abstract class AbstractSocket extends EventEmitter implements WritableSoc
     protected socket? : Net.Socket
     protected heartbeat? : AbstractHeartbeatHandler
     protected shouldReconnect : boolean
-    // protected readonly log : Logger
+    protected readonly log : LoggerContract
 
-    protected constructor(host : string, port : number, shouldReconnect  = true) {
+    protected constructor(host : string, port : number, shouldReconnect  = true, logger:LoggerContract = null) {
         super()
         this.host            = host
         this.port            = port
         this.shouldReconnect = shouldReconnect
-        // this.log             = getLogger([AbstractSocket.name, this.host, this.port])
+        this.log             = logger ?? new NullLogger()
     }
 
     public async connect() {
-        // this.log.debug('Attempting to make a connection')
+        this.log.debug('Attempting to make a connection')
 
         return new Promise((res, rej) => {
             if (this.socket) {
                 const message = 'Socket already exists'
 
-                // this.log.error(message)
+                this.log.error(message)
 
                 return rej(new Error(message))
             }
@@ -36,7 +36,7 @@ export abstract class AbstractSocket extends EventEmitter implements WritableSoc
             const timeout = setTimeout(() => {
                 const message = 'Connection timeout reached'
 
-                // this.log.error(message)
+                this.log.error(message)
 
                 rej(new TimeoutException(message))
             }, 1000)
@@ -59,7 +59,7 @@ export abstract class AbstractSocket extends EventEmitter implements WritableSoc
 
     public disconnect() {
         if (this.socket) {
-            // this.log.info('Forced a disconnection')
+            this.log.info('Forced a disconnection')
             this.shouldReconnect = false
             this.socket.end()
             this.socket = undefined
@@ -77,7 +77,7 @@ export abstract class AbstractSocket extends EventEmitter implements WritableSoc
                 this.restartHeartbeat()
 
                 if (error) {
-                    // this.log.error(`Data written with an error - ${ error.message }`)
+                    this.log.error(`Data written with an error - ${ error.message }`)
 
                     return rej(error)
                 }
@@ -88,26 +88,25 @@ export abstract class AbstractSocket extends EventEmitter implements WritableSoc
     }
 
     protected async onDisconnect() {
-        // this.log.debug('Connection closed')
+        this.log.debug('Connection closed')
 
         this.socket = undefined
 
         if (this.shouldReconnect) {
-            // this.log.info('Automatically reconnecting', {shouldReconnect: this.shouldReconnect})
+            this.log.info('Automatically reconnecting', {shouldReconnect: this.shouldReconnect})
             return await this.connect()
         }
 
-        // this.log.info('Not reconnecting', {shouldReconnect: this.shouldReconnect})
+        this.log.info('Not reconnecting', { shouldReconnect: this.shouldReconnect })
     }
 
-    // @ts-ignore
     protected onError(error : Error) {
         this.shouldReconnect = false
-        // this.log.error(`An error occurred - ${ error.message }`)
+        this.log.error(`An error occurred - ${ error.message }`)
     }
 
     protected async onConnect(socket : Net.Socket) {
-        // this.log.debug('Connection established')
+        this.log.debug('Connection established')
         this.socket          = socket
         this.shouldReconnect = true
         this.restartHeartbeat()
