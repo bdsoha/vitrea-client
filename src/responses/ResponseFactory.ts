67dfb1c12ab03567b5985c2421eed6461f55c93a
Dov Benyomin Sohacheski <b@kloud.email>
2023-01-09 07:13:44 +0000

    🏭 Added factory versioning, closes #5

--- src/responses/ResponseFactory.ts
+++ src/responses/ResponseFactory.ts
@@ -1,39 +1,51 @@
 import { CommandID }              from './ResponseCodes'
+import { ProtocolVersion }        from '../utilities/ProtocolVersion'
 import { DataGramDirection }      from '../utilities/Enums'
 import { BaseResponse, DataGram } from '../core'
 import * as Responses             from '.'
 
-export class ResponseFactory {
-    protected static readonly lookupTable : Record<CommandID, typeof BaseResponse> = {
-        [CommandID.Acknowledgement]:      Responses.Acknowledgement,
-        [CommandID.InternalUnitStatuses]: Responses.InternalUnitStatuses,
-        [CommandID.KeyParameters]:        Responses.KeyParameters,
-        [CommandID.KeyStatus]:            Responses.KeyStatus,
-        [CommandID.NodeCount]:            Responses.NodeCount,
-        [CommandID.NodeMetaData]:         Responses.NodeMetaData,
-        [CommandID.RoomCount]:            Responses.RoomCount,
-        [CommandID.RoomMetaData]:         Responses.RoomMetaData,
+const lookup: Record<CommandID, typeof BaseResponse> = {
+    [CommandID.Acknowledgement]:      Responses.Acknowledgement,
+    [CommandID.InternalUnitStatuses]: Responses.InternalUnitStatuses,
+    [CommandID.KeyParameters]:        Responses.KeyParameters,
+    [CommandID.KeyStatus]:            Responses.KeyStatus,
+    [CommandID.NodeCount]:            Responses.NodeCount,
+    [CommandID.NodeMetaData]:         Responses.NodeMetaData,
+    [CommandID.RoomCount]:            Responses.RoomCount,
+    [CommandID.RoomMetaData]:         Responses.RoomMetaData,
 
-        // Ignored Response
-        [CommandID.NodeExistenceStatus]: Responses.GenericUnusedResponse,
+    // Ignored Response
+    [CommandID.NodeExistenceStatus]: Responses.GenericUnusedResponse,
+} as const
+
+const lookupv2: Record<CommandID, typeof BaseResponse> = {
+    ...lookup,
+    [CommandID.NodeMetaData]: Responses.NodeMetaDataV2,
+    [CommandID.RoomMetaData]: Responses.RoomMetaDataV2
+} as const
+
+export class ResponseFactory {
+    protected static readonly lookupTable = {
+        [ProtocolVersion.V1]: lookup,
+        [ProtocolVersion.V2]: lookupv2,
     }
 
-    protected static isIncoming(rawBuffer : Buffer) : boolean {
+    protected static isIncoming(rawBuffer: Buffer): boolean {
         return rawBuffer[DataGram.directionIndex] === DataGramDirection.INCOMING
     }
 
-    protected static lookup(rawBuffer : Buffer) {
+    protected static lookup(rawBuffer: Buffer, version: ProtocolVersion) {
         const commandID = rawBuffer[DataGram.commandIDIndex] as CommandID
 
-        return this.lookupTable[commandID]
+        return this.lookupTable[version][commandID]
     }
 
-    static find<T extends BaseResponse>(rawBuffer : Buffer) : T | void {
+    static find<T extends BaseResponse>(rawBuffer: Buffer, version: ProtocolVersion): T | void {
         if (!this.isIncoming(rawBuffer)) {
             return
         }
 
-        const supplier = this.lookup(rawBuffer)
+        const supplier = this.lookup(rawBuffer, version)
 
         if (supplier) {
             // @ts-ignore
