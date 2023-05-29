ca70937645b2420ca6b02e5b6b362ec1b8f2fd43
Dov Benyomin Sohacheski <b@kloud.email>
2023-05-29 08:33:03 +0300

    wip

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,10 +3,10 @@ import * as Exports from './index'
 
 describe('imports', () => {
     it('exports 3 modules', () => {
-        expect(Exports.keys()).toHaveLength(3)
+        expect(Object.keys(Exports)).toHaveLength(3)
     })
     
     it('contains the requests module', () => {
-        expect(Exports.Requests.keys()).toHaveLength(12)
+        expect(Object.keys(Exports.Requests)).toHaveLength(12)
     })
 })
