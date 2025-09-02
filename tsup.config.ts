979cbb01b7e6c70f8595ee45a46fb79530943eac
Dov Benyomin Sohacheski <b@kloud.email>
2025-09-02 09:30:23 +0300

    🔧 Fixed minification naming issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  minify: true,
-  target: 'es2020',
-  splitting: false,
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    keepNames: true,
+    minify: true,
+    target: 'es2020',
+    splitting: false,
 })
